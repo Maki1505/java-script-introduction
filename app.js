cc863fef4cfe2f0e9f0e180fc75ebb2a89258ba5
Arman Merdzanic <merdzanicarman@gmail.com>
2024-03-17 21:35:17 +0100

    Updated code

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ let userName = "Maki";
 let hobbies = ["Football", "Video Games", "Proggraming"];
 let job = { title: "Developer", place: "Sarajevo", salary: 50000 };
 
-let adultYears = age - 18;
-alert(adultYears);
+let totalAdultYears;
+
+function calculateAdultYears() {
+   return  age - 18;
+}
+
+totalAdultYears = calculateAdultYears();
+alert(totalAdultYears);
+
+age = 28;
+totalAdultYears = calculateAdultYears();
+
+alert(totalAdultYears);
+
 
 
