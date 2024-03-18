34d90c8b7af1a4f033174d8a9dbcba7402a850cf
Arman Merdzanic <merdzanicarman@gmail.com>
2024-03-18 22:50:59 +0100

    Updated Code

--- app.js
+++ app.js
@@ -10,12 +10,18 @@ function calculateAdultYears(userAge) {
 }
 
 totalAdultYears = calculateAdultYears(age);
-alert(totalAdultYears);
+console.log(totalAdultYears);
 
 age = 28;
 totalAdultYears = calculateAdultYears(age);
 
-alert(totalAdultYears);
+console.log(totalAdultYears);
 
 
+let person = {name : "Maki",
+greet() {
+console.log("Hello");
+}
+}
 
+person.greet();
