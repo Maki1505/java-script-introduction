7fe4e042bf9ac131c2ff80adf65cf916519280d9
Arman Merdzanic <merdzanicarman@gmail.com>
2024-03-19 15:59:06 +0100

    Updated code

---
+++ operations.js
@@ -0,0 +1,29 @@
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 / 4);
+console.log(10 * 4);
+console.log(10 % 4);
+//  ovo je djelilac za ostatak npr 10 podijeljeno sa 4 i ova komanda ce dat ostatak
+
+console.log((10 + 3 - 7) * 10);
+
+let result = (10 + 3 - 7) * 10;
+result = 10 * 4;
+result++; //result = result + 1
+result--; //result = result - 1
+
+result += 5; //result = result + 5
+result -= 5; //result = result - 5
+result *= 5; //result = result * 5
+result /= 5; //result = result / 5
+console.log(result);
+
+console.log("Arman" + " " + "Maki");
+console.log("Maki" - "aki"); // nece raditi zato sto ne prepoznaje matematicke operacije kao stringove-textove
+
+let userName = "Maki";
+console.log(userName.length);
+console.log(userName.toUpperCase());
+
+let hobbies = (["Sports" , "Cooking"]);
+console.log(hobbies.length);
